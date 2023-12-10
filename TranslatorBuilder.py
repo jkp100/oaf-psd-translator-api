27d784ec3d77802b411b5188d95cc3b0fa332b5a
Johnny Panasy <137459213+jkp100@users.noreply.github.com>
2023-12-10 15:05:04 -0500

    Update TranslatorBuilder.py
    Marked with Hashtags as builder service is not needed

--- TranslatorBuilder.py
+++ TranslatorBuilder.py
@@ -1,10 +1,10 @@
-from TranslatorService import TranslatorService
-from TranslatorHandler import TranslatorHandler
+#from TranslatorService import TranslatorService
+#from TranslatorHandler import TranslatorHandler
 
 #Builder that create instances of TranslatorService and TranslatorHandler
-class TranslatorBuilder:
-    def build_service(self, api_url):
-        return TranslatorService(api_url)
+#class TranslatorBuilder:
+    #def build_service(self, api_url):
+        #return TranslatorService(api_url)
 
-    def build_handler(self, translator_service):
-        return TranslatorHandler(translator_service)
+    #def build_handler(self, translator_service):
+        #return TranslatorHandler(translator_service)
