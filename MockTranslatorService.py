9ea33367c60fe70b64f7be414de336dfa5c022a2
Johnny Panasy <137459213+jkp100@users.noreply.github.com>
2023-12-10 15:05:49 -0500

    Update MockTranslatorService.py
    Removed specific api urls

--- MockTranslatorService.py
+++ MockTranslatorService.py
@@ -2,10 +2,8 @@
 import requests
 
 class MockTranslatorService:
-    def __init__(self, api_url_detect_lang, api_url_get_lang, api_url_translate_text):
-        self.api_url_detect_lang = api_url_detect_lang
-        self.api_url_get_lang = api_url_get_lang
-        self.api_url_translate_text = api_url_translate_text
-
+    def __init__(self, api_url):
+        self.api_url = api_url
+        
     def translate_text(self, text_to_translate, source_lang, target_lang):
         return f'Mocked translation of "{text_to_translate}" from {source_lang} to {target_lang}'
